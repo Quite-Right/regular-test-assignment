ac53ba2e10109ca9e63a09e16ba57ad040288730
Novogrudskii Dmitrii <novogrudskiy.dv@gmail.com>
2023-02-10 03:48:09 +0300

    fixed eyes display

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,6 +1,6 @@
 export const BASE_URL = 'https://swapi.dev/api';
 
 export const getCharactersUrl = (param: {page?: number, search?: string} = {}) =>
-  `${BASE_URL}/people/${param.search || param.page !== undefined ? '?' : ''}${param.search ?
+  `/people/${param.search || param.page !== undefined ? '?' : ''}${param.search ?
     `search=${param.search}` : ''}${param.search && param.page !== undefined ? '&' : ''}${param.page !== undefined ? `page=${param.page}` : ''}`;
-export const getCharacterUrl = (id: string) => `/${getCharactersUrl()}${id}/`;
+export const getCharacterUrl = (id: string) => `${getCharactersUrl()}${id}/`;
